bbacfc0d6e896d5c3d9354e305f729d33e7b692c
ELLYHC <modestovchylle@gmail.com>
2026-09-15 18:54:28 +0800

    done

---
+++ app/src/test/java/qr2/modesto.java
@@ -0,0 +1,24 @@
+package qr2;
+
+import org.junit.Test;
+
+public class modesto {
+
+    @Test
+    public void printMYprofile(){
+        String myName = "modesto";
+        String petname1 = "itim";
+        String petname2 = "puti";
+        String favfood = "itim";
+        int myAge = 17;
+
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am + myAge + ");
+        System.out.println("I have 2 wonderful pets named" + petname1 + " and " + petname2 + ".");
+        System.out.println("If I could eat " + favfood + " everyday!");
+
+    }
+
+}
+
